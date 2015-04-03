a05c3548279e6fc706d51ddfce4a179d0513a3cb
Lei Zhao <leizha@uber.com>
2015-04-03 16:46:54 -0700

    renamed Result::result to value

--- specs/ret.js
+++ specs/ret.js
@@ -1,12 +1,12 @@
-function Result(error, result) {
-    this.result = result;
+function Result(error, value) {
+    this.value = value;
     this.error = error;
     this.errorCtx = [];
 }
 module.exports.Result = Result;
 
-module.exports.just = function just(result) {
-    return new Result(null, result);
+module.exports.just = function just(value) {
+    return new Result(null, value);
 };
 
 module.exports.error = function just(error) {
